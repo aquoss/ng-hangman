2577534cd1bbeb081afdd27e70c51dc160bb1673
aquoss <amberquoss@gmail.com>
2017-01-11 15:08:09 -0800

    completes hangman assignment along with lowercase stretch goal

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,5 +1,15 @@
 console.log('app.js loaded!');
 
 // initialize the application
-angular
-  .module("hangmanApp", []);
+angular.module('hangmanApp', [])
+       .controller('HangmanController', HangmanController);
+
+HangmanController.$inject = [];
+function HangmanController(){
+  var vm = this;
+  vm.hangman = new HangmanGame('ElEphant');
+  vm.guessLetter = function(input){
+    vm.hangman.guess(input);
+    vm.hangman.input = "";
+  }
+}
